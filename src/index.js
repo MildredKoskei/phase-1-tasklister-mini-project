f7d8812530312f96712cb37bf8254fde9afe90b9
Mildred Chepkoech Koskei <mildredkoskei01@gmail.com>
2022-08-24 15:55:44 -0400

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+let form = document.querySelector('form')
+form.addEventListener('submit', (e)=> {
+
+   e.preventDefault()
+   tasksToDo(e.target.new_task_description.value)
+   
+form.reset()
+})
+
+function tasksToDo(task){
+    console.log(task)
+    let p = document.createElement('p')
+    let btn = document.createElement('button')
+    btn.addEventListener('click', deleteHandle)
+    btn.textContent = 'x'
+     p.textContent = `${task}`
+     p.appendChild(btn)
+    console.log(p)
+     document.querySelector('#list').appendChild(p)
+}
+
+function deleteHandle(e){
+  e.target.parentNode.remove()
+
+  
+}
+})
+
+
+
+
+
+
